87e977826a8d08877437366165e825c3c6417b77
R0ner <runesehested.rs@gmail.com>
2022-01-14 18:12:11 +0100

    Added draw boxes option and optimized function.

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -32,8 +32,12 @@ class PredictSet:
         self.is_url = False
         if os.path.exists(self.images_from):
             if os.path.isdir(self.images_from):
-                self.load_from = [f'{self.images_from}/{filename}' for filename in next(os.walk(self.images_from),
-                                      (None, None, []))[2]]
+                self.load_from = [
+                    f'{self.images_from}/{filename}'
+                    for filename in next(os.walk(self.images_from), (None,
+                                                                     None,
+                                                                     []))[2]
+                ]
             elif os.path.isfile(self.images_from):
                 self.load_from.append(self.images_from)
         else:
@@ -53,9 +57,8 @@ class PredictSet:
             if self.is_url:
                 try:
                     id = self.load_from.pop(0)
-                    image = Image.open(
-                        requests.get(id,
-                                     stream=True).raw).convert('RGB')
+                    image = Image.open(requests.get(
+                        id, stream=True).raw).convert('RGB')
                 except:
                     log.debug(f'Failed to load image from url: {id}')
                     continue
@@ -67,7 +70,7 @@ class PredictSet:
                     log.debug(f'Failed to load image from file: {id}')
                     continue
             yield image, id
-            
+
 
 @click.command()
 @click.argument('load_model_from', type=click.Path(exists=True))
@@ -101,9 +104,19 @@ def predict(load_model_from,
         "mishig/tiny-detr-mobilenetsv3",
         num_labels=n_classes,
         ignore_mismatched_sizes=True)
-    
+
     Images = PredictSet(images_from)
     assert Images, "No images were found."
+
+    # Initialize {predictions_to} folder
+    if os.path.exists(predictions_to):
+        dirs = next(os.walk(predictions_to))[1]
+        dirs_numerated = [int(dir) for dir in dirs]
+        next_dir = str(max(dirs_numerated) + 1)
+    else:
+        os.mkdir(predictions_to)
+        next_dir = str(1)
+    os.mkdir(f'{predictions_to}/{next_dir}')
 
     # Save configuration info and results.
     ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -115,7 +128,6 @@ def predict(load_model_from,
     }
     results = []
 
-    images_out = []
     i = 0
     # Prediction
     while len(Images):
@@ -125,10 +137,12 @@ def predict(load_model_from,
         batch_images_out = []
         while len(batch) < batch_size and len(Images):
             image, id = next(Images.loader)
-            batch_images_out.append(image.copy())
+            if draw_boxes:
+                batch_images_out.append(image.copy())
             batch.append(image)
             batch_results.append({'id': i, 'file_name': id, 'detections': []})
             i += 1
+        bsize = len(batch) # Actual batch size
 
         encoding = feature_extractor(images=batch, return_tensors='pt')
 
@@ -136,50 +150,47 @@ def predict(load_model_from,
             outputs = model(**encoding)
 
         # We loop over each image in the batch
-        for logits, bboxes, image_out, result in zip(outputs.logits,
+        for logits, bboxes, result in zip(outputs.logits,
                                                      outputs.pred_boxes,
-                                                     batch_images_out,
                                                      batch_results):
-            image_w, image_h = image_out.size
-            draw = ImageDraw.Draw(image_out, "RGBA")
             probs = softmax(logits, dim=1)
             best = np.argmax(probs, 1)
             criteria = probs[np.arange(len(best)), best] > threshold
             for pred, box, prob in zip(best[criteria], bboxes[criteria],
                                        probs[criteria]):
-                pred_class = classes[pred] if pred < n_classes else "No Object"
+                # If 'No Object' is detected, skip over the detection
+                if pred < n_classes:
+                    pred_class = classes[pred]
+                else:
+                    continue
+                
                 confidence = prob[pred].item()
                 x, y, w, h = tuple(box)
                 x, y, w, h = x.item(), y.item(), w.item(), h.item()
                 result['detections'].append(
-                    (pred_class, (x, y, w, h), confidence))
-
-                x *= image_w
-                y *= image_h
-                w *= image_w
-                h *= image_h
-                draw.rectangle((x - w / 2, y - h / 2, x + w / 2, y + h / 2),
-                               outline='red',
-                               width=2)
-                draw.text((x - w / 2 + 5, y - h / 2 + 5),
-                          pred_class,
-                          fill='red')
-            images_out.append(image_out)
+                    (pred_class, (x, y, w, h), confidence)) 
             results.append(result)
+        
+        if draw_boxes:
+            for j, image_out in enumerate(batch_images_out):
+                idx = i - bsize + j
+                image_w, image_h = image_out.size
+                draw = ImageDraw.Draw(image_out, "RGBA")
+                for detection in results[idx]['detections']:
+                    pred_class, (x, y, w, h), confidence = detection
+                    x *= image_w
+                    y *= image_h
+                    w *= image_w
+                    h *= image_h
+                    draw.rectangle(
+                        (x - w / 2, y - h / 2, x + w / 2, y + h / 2),
+                        outline='red',
+                        width=2)
+                    draw.text((x - w / 2 + 5, y - h / 2 + 5),
+                                pred_class,
+                                fill='red')
+                    image_out.save(f'{predictions_to}/{next_dir}/im_pred{idx}.jpg')
     annotated['results'] = results
-
-    # Save results to {predictions_to} folder
-    if os.path.exists(predictions_to):
-        dirs = next(os.walk(predictions_to))[1]
-        dirs_numerated = [int(dir) for dir in dirs]
-        next_dir = str(max(dirs_numerated) + 1)
-    else:
-        os.mkdir(predictions_to)
-        next_dir = str(1)
-    os.mkdir(f'{predictions_to}/{next_dir}')
-
-    for i, image_out in enumerate(images_out):
-        image_out.save(f'{predictions_to}/{next_dir}/im_pred{i}.jpg')
 
     # Save info to a readable json file.
     with open(f'{predictions_to}/{next_dir}/annotated.json', 'w') as file:
@@ -190,6 +201,6 @@ def predict(load_model_from,
 
 if __name__ == '__main__':
     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.DEBUG, format=log_fmt)
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     predict()
