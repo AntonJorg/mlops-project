61a17f8a37298f12b73c51f3e10936cf1f8c4b3e
s175548 <s175548@student.dtu.dk>
2022-01-18 15:57:28 +0100

    asd

--- trigger_test.py
+++
@@ -1,8 +0,0 @@
-import time
-import requests
-url = 'https://us-central1-dtumlops-detr.cloudfunctions.net/function-1'
-payload = {'message': 'Hello, General Kenobi'}
-
-for _ in range(1000):
-   r = requests.get(url, params=payload)
-
